11937742a7beee110f2ad4e63dda559df4cd20d2
febin-varghese <febinvarghese994@gmail.com>
2021-01-28 00:37:46 +0100

    Test dashboard creation

---
+++ app.py
@@ -0,0 +1,18 @@
+import dash
+# from dash.dependencies import Input, Output
+# import dash_core_components as dcc
+import dash_html_components as html
+
+import flask
+import os
+
+server = flask.Flask('app')
+server.secret_key = os.environ.get('secret_key', 'secret')
+
+app = dash.Dash('app', server=server)
+
+app.layout = html.Div([html.H1("Simply Optimize")])
+
+
+if __name__ == '__main__':
+    app.run_server()
