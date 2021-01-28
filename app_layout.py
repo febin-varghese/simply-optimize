9556d84b1bafeae0b8efb5020127e52d1aa957e6
febin-varghese <febinvarghese994@gmail.com>
2021-01-28 17:30:38 +0100

    minor changes

--- app_layout.py
+++ app_layout.py
@@ -1,10 +1,8 @@
-import dash
-# from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 
 # User defined modules
-from test_functions import Ackley
 from test_functions import available_functions
 
 
@@ -18,17 +16,28 @@ def set_plot_children(name):
 
 
 def app_layout(name="Ackley"):
+    """
+    Layout of the application
+    :param name: Name of the test function
+    :return: Layout
+    """
     test_fns = available_functions()
     fn_options = [{'label': key, 'value': key} for key in test_fns.keys()]
+    alg_options = [{'label': "WIP", 'value': "1"}]
     layout = html.Div([
         html.H1("Simply Optimize", style={'textAlign': 'center', 'color': '#40D3F3'}),
         html.Div(id='plot-layer', style={'width': '80%', 'height': '100%', 'display': 'block', 'margin-left': 'auto',
                                          'margin-right': 'auto'}, children=set_plot_children(name)
                  ),
-        html.Div(style={'width': '80%', 'height': '100%', 'display': 'block', 'margin-left': 'auto',
-                        'margin-right': 'auto'},
-                 children=[dcc.Dropdown(id='function-dropdown', options=fn_options, value=fn_options[0]['value'],
-                                        clearable=False)],
+        html.Div(className="row", style={'width': '80%', 'height': '100%', 'display': 'block', 'margin-left': 'auto',
+                                         'margin-right': 'auto'},
+                 children=[
+                     html.Div([dcc.Dropdown(id='function-dropdown', options=fn_options, value=fn_options[0]['value'],
+                                            clearable=False)
+                               ], className="six columns", style={'display': 'block'}),
+                     html.Div([dcc.Dropdown(id='algorithm-dropdown', options=alg_options, value=alg_options[0]['value'],
+                                            clearable=False)
+                               ], className="six columns", style={'display': 'block'})],
                  )
 
     ])
